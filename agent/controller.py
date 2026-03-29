0f8a8eca29cfb3ce8da9c726298fee994d5fca9e
Broyojo <dha@xoba.com>
2026-03-29 13:34:26 -0400

    Revert "Skip rollout on confident roots"
    This reverts commit e33f66d5a40d8463f16f51e77de49f3a6fc7fda9.

--- agent/controller.py
+++ agent/controller.py
@@ -893,11 +893,7 @@ class PlayerController:
             max_candidates = 0
 
         # Use simulation if we have time and multiple candidates
-        confident_root = (
-            len(candidates) >= 2
-            and (candidates[0][0] - candidates[1][0]) >= 6.0
-        )
-        if sim_turns > 0 and len(candidates) > 1 and not confident_root:
+        if sim_turns > 0 and len(candidates) > 1:
             move_dir = self._simulate_candidates(board, me, player_parity,
                                                  candidates, opp,
                                                  sim_turns, max_candidates)
